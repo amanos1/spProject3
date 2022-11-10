f04fbce749e285fb1fcf2b6abb28481c7c30b68c
thing-maker <ajb445@scarletmail.rutgers.edu>
2022-11-09 19:41:53 -0500

    alright the baseline functions work pretty good, now it's time for the garder stuff

--- shell.c
+++ shell.c
@@ -1,40 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <wait.h>
 
-void runLine(char *line) {
-	char spaceBar[2] = " ";
-	char *each;
+/*************************************************************/
+/* This program is a crude implementation of a linnux shell. */
+/* It can do many of the basic things a shell can.           */
+/*                                                           */
+/* Date: Nov 18, 2022                                        */
+/*************************************************************/
 
-	each = strtok(line, spaceBar);
-
-	if(access(each, F_OK) != 0) {
-		printf("%s: No such file or directory\n", each);
-		return;
-	}
-
-	size_t max = 2;
-	int i;
-	char **args = malloc(sizeof(char *) * max);
-	char *env[] = { (char *)0 };
-
-	for(i = 0; each != NULL; i++) {
-		if(i + 1 >= max) {
-			max *= 2;
-			args = realloc(args, sizeof(char *) * max);
-		}
-
-		args[i] = each;
-
-		each = strtok(NULL, spaceBar);
-	}
-
-	args[i] = NULL;
-	execve(args[0], args, env);
-
-	free(args);
-}
+void runLine(char *line);
 
 int main() {
 	char *werd = malloc(2);
@@ -49,17 +27,20 @@ int main() {
 	int end = 0;
 	size_t buf = 2;
 
+	//read strings until we reach an EOF
 	while(1) {
 		size_t len = 0;
 
 		printf("> ");
 
+		//reads the string one character at a time
 		while (1) {
 			if((c = getchar()) == EOF) {
 				end = 1;
 				break;
 			} else if(c == '\n') break;
 
+			//resize the string if it exceeds the current maximum length
 			if (len + 1 == buf) {
 				buf *= 2;
 				newWerd = realloc(werd, buf);
@@ -80,10 +61,63 @@ int main() {
 
 		werd[len] = '\0';
 
-		runLine(werd);
+		if(fork() == 0) {
+			runLine(werd);
+			end = 1;
+			break;
+		} else {
+			wait(NULL);
+		}
 	}
 
 	free(werd);
 
 	return 0;
+}
+
+/********************************************/
+/* Processes the  command the user inputted */
+/********************************************/
+void runLine(char *line) {
+	char spaceBar[2] = " ";
+	char *each;
+
+	each = strtok(line, spaceBar);
+
+	if(access(each, F_OK) != 0) {
+		char *newShit = malloc(10 + strlen(each));
+		strcpy(newShit, "/bin/");
+		strcat(newShit, each);
+		if(access(newShit, F_OK) != 0) {
+			strcpy(newShit, "/usr/bin/");
+			strcat(newShit, each);
+			if(access(newShit, F_OK) != 0) {
+				printf("%s: No such file or directory\n", each);
+				free(newShit);
+				return;
+			}
+		}
+		free(newShit);
+	}
+
+	size_t max = 2;
+	int i;
+	char **args = malloc(sizeof(char *) * max);
+	//char *env[] = { (char *)0 };
+
+	for(i = 0; each != NULL; i++) {
+		if(i + 1 >= max) {
+			max *= 2;
+			args = realloc(args, sizeof(char *) * max);
+		}
+
+		args[i] = each;
+
+		each = strtok(NULL, spaceBar);
+	}
+
+	args[i] = NULL;
+	execvp(args[0], args);
+
+	free(args);
 }
