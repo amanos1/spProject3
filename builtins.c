9413d60b5ed420006b9d11598af725d99cdf3bbf
thing-maker <ajb445@scarletmail.rutgers.edu>
2022-11-15 13:26:08 -0500

    sjdfnsk

--- builtins.c
+++ builtins.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* ListOfCommands[4];
+char* ListOfCommands[6];
 
 ListOfCommands[0] = "bg";
 ListOfCommands[1] = "cd";
@@ -13,45 +13,30 @@ ListOfCommands[4] = "jobs";
 ListOfCommands[5] = "kill";
 
 
-//Find a builtin
-int lookup(char* cmd){
-	for(int i = 0; i < 6; i++){
-		if(strcmp(ListOfCommands[i], cmd) == 0){
-			//Call function ?? idk how we can pass just the first part
-			//and then the additional
-		}
-	}
-	return 0;
-}
-
-
-
 //Run a suspended job in background
 int background(char* jobId){
-
-
-
 }
+
 //Change directory
 int changeDir(char* path){
 	//Given absolute path
 	if(path[0] == '/'){
 		if(chdir(path) == -1){
-			printf("%s : %s\n", path, sterror(errno);
+			printf("%s : %s\n", path, sterror(errno));
 		}
 		setenv("PWD", path, 1);
 	}
 	//Given relative path
 	else if(path[0] == '.'){
 		if(chdir(path) == -1){
-			printf("%s : %s\n", path, sterror(errno);
+			printf("%s : %s\n", path, sterror(errno));
 		}
 		setenv("PWD", path, 1);
 	}
 	//Given no path
 	else if(path == NULL){
 		if(chdir(getenv("HOME")) == -1){
-			printf("%s : %s\n", path, sterror(errno);
+			printf("%s : %s\n", path, sterror(errno));
 		}
 		setenv("PWD", getenv("HOME"));
 	}
@@ -85,3 +70,28 @@ int kill(pid_t pid){
 
 	return 0;
 }
+
+//Find a builtin
+int lookup(char** cmd){
+	if(strcmp(ListOfCommands[0], cmd[0]) == 0){
+		background(cmd);
+		return 1;
+	} if(strcmp(ListOfCommands[1], cmd[0]) == 0){
+		changeDir(cmd);
+		return 1;
+	} if(strcmp(ListOfCommands[2], cmd[0]) == 0){
+		exit();
+		return 1;
+	} if(strcmp(ListOfCommands[3], cmd[0]) == 0){
+		foreground(cmd);
+		return 1;
+	} if(strcmp(ListOfCommands[4], cmd[0]) == 0){
+		jobs();
+		return 1;
+	} if(strcmp(ListOfCommands[5], cmd[0]) == 0){
+		kill(cmd);
+		return 1;
+	}
+	return 0;
+}
+
